2c80bcb13e7cbc285f096e7fa79152e676f2618d
warthog <853996234hu@gmail.com>
2020-12-02 18:03:19 +0800

    asD

---
+++ src/main/java/com/bjfu/fortree/BjfuFortreeApplication.java
@@ -0,0 +1,13 @@
+package com.bjfu.fortree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjfuFortreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BjfuFortreeApplication.class, args);
+	}
+
+}
